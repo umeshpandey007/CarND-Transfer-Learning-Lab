53587f757952cb371c99005cf35bfef89d8337d4
Umesh Pandey <umeshpandey007@gmail.com>
2017-05-01 02:07:04 -0700

    Transfer Learning for VGG, Resnet & GoogLENet

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,9 @@
 import pickle
 import tensorflow as tf
-# TODO: import Keras layers you need here
+#import Keras layers you need here
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
+import numpy as np
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,6 +11,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_integer('epochs', 50, "Number of Epochs")
+flags.DEFINE_integer('batch_size', 256, "Batch size")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -41,14 +46,20 @@ def main(_):
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
-    # TODO: define your model and hyperparams here
+    # define your model and hyperparams here
     # make sure to adjust the number of classes based on
     # the dataset
     # 10 for cifar10
     # 43 for traffic
-
-    # TODO: train your model here
-
+    num_classes = len(np.unique(y_train))
+    # train your model here
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(num_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, nb_epoch=FLAGS.epochs, batch_size=FLAGS.batch_size, validation_data=[X_val, y_val] )
 
 # parses flags and calls the `main` function above
 if __name__ == '__main__':
